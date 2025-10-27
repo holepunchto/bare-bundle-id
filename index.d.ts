eb8b1cdd0c56ee81cbc4717dcada7433790dee8e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-10-27 09:58:22 +0100

    Remove internal argument from typing

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import Bundle from 'bare-bundle'
 import Buffer from 'bare-buffer'
 
-declare function id(bundle: Bundle, out?: Buffer): Buffer
+declare function id(bundle: Bundle): Buffer
 
 export = id
